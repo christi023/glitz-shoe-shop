9f08f5a66b85fda468d81462e0fb5dd8c9cec58c
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-07-28 20:22:12 +0200

    Navbar added

---
+++ src/Components/Navbar/NavBar.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import logo from '../../assets/bag-front-view.png';
+import navData from '../../data/NavData';
+
+// style
+import './NavBar.css';
+
+const NavBar = () => {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+      <Container>
+        <NavLink to="/">
+          <img src={logo} alt="logo" style={{ width: '50px', marginBottom: '1em' }} />
+          <Navbar.Brand> GLITZ</Navbar.Brand>
+        </NavLink>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            {navData.map((val, i) => (
+              <NavLink
+                to={val.link}
+                key={i}
+                className="nav-menu-links"
+                activeClassName="active_class"
+              >
+                {val.title}
+                <img src={val.img} alt="" className="shopping-icon" />
+              </NavLink>
+            ))}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
