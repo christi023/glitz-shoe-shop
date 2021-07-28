b6350252a4256f3d5644489659ce471b74a40ab2
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-07-28 20:23:09 +0200

    Holds all data links and data for componenents and pages

---
+++ src/data/NavData.jsx
@@ -0,0 +1,30 @@
+import cart from '../assets/shop_icon.png';
+
+const navData = [
+  {
+    title: 'Home',
+    link: '/',
+  },
+  {
+    title: 'Collection',
+    link: '/collection',
+  },
+  {
+    title: 'Shoes',
+    link: '/shoes',
+  },
+  {
+    title: 'Contact ',
+    link: '/contact',
+  },
+  {
+    link: '/cart',
+    img: cart,
+  },
+  {
+    title: 'Register',
+    link: '/register',
+  },
+];
+
+export default navData;
