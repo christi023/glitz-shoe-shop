188b56f7e4cdd283259fe27dbec85bbcd372742c
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-07-30 15:35:40 +0200

    Holds components & Pages data

---
+++ src/data/CarrouselData.jsx
@@ -0,0 +1,28 @@
+import img1 from '../assets/running-shoes.png';
+
+export const objOne = {
+  h1: 'New Running Shoes',
+  h2: `Men's Plex`,
+  p: 'ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  button: 'Buy Now',
+  button2: 'See More',
+  img: img1,
+};
+
+export const objTwo = {
+  h1: 'Athletic Shoes',
+  h2: `Women's Plex`,
+  p: 'ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  button: 'Buy Now',
+  button2: 'See More',
+  img: img1,
+};
+
+export const objThree = {
+  h1: 'All Round Shoes',
+  h2: `Kid's Plex`,
+  p: 'ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  button: 'Buy Now',
+  button2: 'See More',
+  img: img1,
+};
